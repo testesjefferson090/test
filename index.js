c9c3393e2a0318d4b0831bb026f70e72f9b38971
testesjefferson090 <65197771+testesjefferson090@users.noreply.github.com>
2022-03-25 09:18:20 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+const app = require('express')()
+
+app.get('/', (req, res) => {
+	res.send('Hello World!')
+})
+
+app.listen(process.env.PORT || 3000, () => {
+	console.log('Servidor aberto na porta', process.env.PORT || 3000)
+})
